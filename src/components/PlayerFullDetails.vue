a1958ab0d53e83012698a46c248429ce74fa3a09
guy <guyklinger94@gmail.com>
2021-06-11 17:23:27 +0300

    players full details

---
+++ src/components/PlayerFullDetails.vue
@@ -0,0 +1,99 @@
+<template>
+<div class="player-preview">
+    <div :title="name" class="player-title">
+        <b>Player Name:</b> {{ name }}
+        <br/>
+        <b>Player Id:</b> {{ player_id }}
+    </div>
+    <br/>
+    <ul class="player-content">
+      <li> Common Name: {{ common_name }}</li>
+      <li> Image: <img src="${image}"/></li>
+      <li> Position: {{ position }}</li>
+      <li> Team Name: {{ team_name }}</li>
+      <li> Birth Date: {{ birth_date }}</li>
+      <li> Birth Country: {{ birth_country }}</li>
+      <li> Height: {{ height }}</li>
+      <li> Weight: {{ weight }}</li>
+      <li> Nationality: {{ nationality }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlayerPreview",
+  props: {
+      player_id: {
+        type: Number,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      common_name: {
+        type: String,
+        required: true
+      },
+      image: {
+        type: String,
+        required: true
+      },
+      position: {
+        type: Number,
+        required: true
+      },
+      team_name: {
+        type: String,
+        required: true
+      },
+      birth_date: {
+        type: String,
+        required: true
+      },
+      birth_country: {
+        type: String,
+        required: true
+      },
+      height: {
+        type: String,
+        required: true
+      },
+      weight: {
+        type: String,
+        required: true
+      },
+      nationality: {
+        type: String,
+        required: true
+      },
+  },
+}
+</script>
+
+<style>
+.player-preview {
+  display: inline-block;
+  width: 400px;
+  height: 400px;
+  position: relative;
+  margin: 10px 10px;
+  border-style: solid;
+  border-radius: 10px;
+  border-width: 5px;
+  border-color:cadetblue;
+}
+
+.player-preview .player-title {
+  text-align: center;
+  text-transform: uppercase;
+  color:  rgb(111, 197, 157);
+}
+
+.player-preview .player-content {
+  width: 100%;
+  overflow: hidden;
+  font-weight: bold
+}
+</style>
---
+++ src/pages/PlayerPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+      <PlayerFullDetails :player_id="player.player_id" :name="player.name"
+       :common_name="player.common_name" :image="player.image" 
+       :position="player.position" :team_name="player.team_name"
+      :birth_date="player.birth_date" :birth_country="player.birth_country" :height="player.height"
+      :weight="player.weight" :nationality="player.nationality">
+      </PlayerFullDetails>
+
+  </div>
+</template>
+
+<script>
+import PlayerFullDetails from '../components/PlayerFullDetails'
+export default {
+    components: {
+        PlayerFullDetails
+    },
+    data() {
+        return {
+            player: {}
+        }
+    },
+
+    methods: {
+        async getPlayer(){
+            let playerId = this.$store.state.players[this.$store.state.currentPlayer]
+            const response = await this.axios.get(
+            `${this.$store.state.server_domain}players/playerFullDetails/${playerId}`,
+            );
+            console.log(response.data)
+            this.player = response.data[0];
+        }
+    },
+    mounted(){
+        console.log("player page mounted");
+        this.getPlayer()
+    },
+}
+</script>
+
+<style>
+
+</style>
